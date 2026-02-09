973122a27fa5a7038419ed9b39699c3145b63c07
Takeru KODAMA <zen.en.tonal@gmail.com>
2026-02-09 17:43:56 +0900

    Update test

--- test/mortar/storage/local_test.exs
+++ test/mortar/storage/local_test.exs
@@ -7,6 +7,19 @@ defmodule Mortar.Storage.LocalTest do
   @test_key "abcdef1234567890abcdef1234567890"
   @test_value "test binary data"
 
+  defp binary_to_stream(binary) when is_binary(binary) do
+    {:ok, pid} = StringIO.open(binary)
+    IO.binstream(pid, :line)
+  end
+
+  defp stream_to_binary(stream) do
+    stream
+    |> Enum.to_list()
+    |> to_string()
+  end
+
+  defp test_stream(), do: nil
+
   setup do
     # Clean up test storage before each test
     File.rm_rf!(@test_storage_path)
@@ -20,53 +33,45 @@ defmodule Mortar.Storage.LocalTest do
     end
 
     test "returns {:ok, value} when key exists" do
-      :ok = Local.put(@test_key, @test_value)
-      assert {:ok, @test_value} = Local.get(@test_key)
-    end
-
-    test "returns binary data as-is" do
-      binary_data = <<1, 2, 3, 4, 5>>
-      :ok = Local.put(@test_key, binary_data)
-      assert {:ok, ^binary_data} = Local.get(@test_key)
+      :ok = Local.put(@test_key, @test_value |> binary_to_stream())
+      assert {:ok, stream} = Local.get(@test_key)
+      assert stream_to_binary(stream) == @test_value
     end
   end
 
   describe "put/2" do
     test "stores key-value pair successfully" do
-      assert :ok = Local.put(@test_key, @test_value)
-      assert {:ok, @test_value} = Local.get(@test_key)
+      assert :ok = Local.put(@test_key, binary_to_stream(@test_value))
+      assert {:ok, stream} = Local.get(@test_key)
+      assert stream_to_binary(stream) == @test_value
     end
 
     test "creates directory structure automatically" do
       key = "abcd1234567890abcdef1234567890ab"
-      :ok = Local.put(key, "data")
+      :ok = Local.put(key, binary_to_stream("some data"))
 
       expected_path = Path.join([@test_storage_path, "ab", "cd", "1234567890abcdef1234567890ab"])
       assert File.exists?(expected_path)
     end
 
     test "is idempotent - can put same value multiple times" do
-      assert :ok = Local.put(@test_key, @test_value)
-      assert :ok = Local.put(@test_key, @test_value)
-      assert {:ok, @test_value} = Local.get(@test_key)
+      assert :ok = Local.put(@test_key, @test_value |> binary_to_stream())
+      assert :ok = Local.put(@test_key, @test_value |> binary_to_stream())
+      assert {:ok, stream} = Local.get(@test_key)
+      assert stream_to_binary(stream) == @test_value
     end
 
     test "overwrites existing value" do
-      :ok = Local.put(@test_key, "old value")
-      :ok = Local.put(@test_key, "new value")
-      assert {:ok, "new value"} = Local.get(@test_key)
-    end
-
-    test "handles binary data correctly" do
-      binary_data = :crypto.strong_rand_bytes(1024)
-      assert :ok = Local.put(@test_key, binary_data)
-      assert {:ok, ^binary_data} = Local.get(@test_key)
+      :ok = Local.put(@test_key, binary_to_stream("old value"))
+      :ok = Local.put(@test_key, binary_to_stream("new value"))
+      assert {:ok, stream} = Local.get(@test_key)
+      assert stream_to_binary(stream) == "new value"
     end
   end
 
   describe "delete/1" do
     test "deletes existing key successfully" do
-      :ok = Local.put(@test_key, @test_value)
+      :ok = Local.put(@test_key, binary_to_stream(@test_value))
       assert :ok = Local.delete(@test_key)
       assert {:error, :not_found} = Local.get(@test_key)
     end
@@ -80,19 +85,19 @@ defmodule Mortar.Storage.LocalTest do
       key1 = "aabbccddaabbccddaabbccddaabbccdd"
       key2 = "aabbccdd11223344aabbccdd11223344"
 
-      :ok = Local.put(key1, "value1")
-      :ok = Local.put(key2, "value2")
+      :ok = Local.put(key1, binary_to_stream("value1"))
+      :ok = Local.put(key2, binary_to_stream("value2"))
 
       assert :ok = Local.delete(key1)
       assert {:error, :not_found} = Local.get(key1)
-      assert {:ok, "value2"} = Local.get(key2)
+      assert {:ok, _} = Local.get(key2)
     end
   end
 
   describe "directory structure" do
     test "uses first 2 chars and second 2 chars for directory hierarchy" do
       key = "12345678901234567890123456789012"
-      :ok = Local.put(key, "data")
+      :ok = Local.put(key, binary_to_stream("some data"))
 
       expected_path = Path.join([@test_storage_path, "12", "34", "5678901234567890123456789012"])
       assert File.exists?(expected_path)
@@ -106,7 +111,7 @@ defmodule Mortar.Storage.LocalTest do
       ]
 
       for key <- keys do
-        :ok = Local.put(key, "data for #{key}")
+        :ok = Local.put(key, binary_to_stream("value_#{key}"))
       end
 
       # Verify all files exist
@@ -129,7 +134,7 @@ defmodule Mortar.Storage.LocalTest do
 
       tasks =
         for key <- keys do
-          Task.async(fn -> Local.put(key, "value_#{key}") end)
+          Task.async(fn -> Local.put(key, binary_to_stream("value_#{key}")) end)
         end
 
       results = Task.await_many(tasks)
@@ -137,7 +142,8 @@ defmodule Mortar.Storage.LocalTest do
 
       for key <- keys do
         expected = "value_#{key}"
-        assert {:ok, ^expected} = Local.get(key)
+        assert {:ok, stream} = Local.get(key)
+        assert stream_to_binary(stream) == expected
       end
     end
   end
